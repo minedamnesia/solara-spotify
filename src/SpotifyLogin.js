75f20881ac8b4215f4b84e87d212d2c2e1235680
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-07-08 16:57:51 -0700

    redirect immediately

--- src/SpotifyLogin.js
+++ src/SpotifyLogin.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { generateCodeChallenge, generateCodeVerifier } from './pkceUtils';
 
-// Spotify app settings
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = 'https://solara-spotify.vercel.app/callback';
-const scopes = [
+const redirectUri = window.location.origin + '/callback';
+const scope = [
   'playlist-read-private',
   'playlist-read-collaborative',
   'streaming',
@@ -12,53 +12,24 @@ const scopes = [
   'user-read-currently-playing'
 ].join(' ');
 
-// PKCE helper functions
-function generateCodeVerifier(length = 128) {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~';
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-async function generateCodeChallenge(codeVerifier) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(codeVerifier);
-  const digest = await window.crypto.subtle.digest('SHA-256', data);
-  return btoa(String.fromCharCode(...new Uint8Array(digest)))
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=+$/, '');
-}
-
 export default function SpotifyLogin() {
-  const handleLogin = async () => {
+  useEffect(() => {
     const codeVerifier = generateCodeVerifier();
-    const codeChallenge = await generateCodeChallenge(codeVerifier);
+    generateCodeChallenge(codeVerifier).then((codeChallenge) => {
+      localStorage.setItem('spotify_code_verifier', codeVerifier);
 
-    // Save the verifier to localStorage for the callback
-    localStorage.setItem('spotify_code_verifier', codeVerifier);
+      const params = new URLSearchParams({
+        response_type: 'code',
+        client_id: clientId,
+        scope: scope,
+        redirect_uri: redirectUri,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge
+      });
 
-    // Build Spotify authorization URL
-    const authUrl = `https://accounts.spotify.com/authorize?` +
-      `client_id=${clientId}` +
-      `&response_type=code` +
-      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-      `&scope=${encodeURIComponent(scopes)}` +
-      `&code_challenge_method=S256` +
-      `&code_challenge=${codeChallenge}`;
+      window.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    });
+  }, []);
 
-    // Redirect to Spotify
-    window.location.href = authUrl;
-  };
-
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <button onClick={handleLogin} className="p-4 bg-green-600 text-white rounded-2xl hover:bg-green-700">
-        Login with Spotify
-      </button>
-    </div>
-  );
+  return <div className="text-center p-8 text-gray-800">Redirecting to Spotify...</div>;
 }
-
